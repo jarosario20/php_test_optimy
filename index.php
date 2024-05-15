ce5c89a5c3ccab1cab60b4a022bf9521136b6aa5
Jay-Ar <jay.rosario024@gmail.com>
2024-05-16 00:28:38 +0800

    optimized index.php

--- index.php
+++ index.php
@@ -1,18 +1,43 @@
 <?php
-
 define('ROOT', __DIR__);
-require_once(ROOT . '/utils/NewsManager.php');
-require_once(ROOT . '/utils/CommentManager.php');
+require_once ROOT . '/vendor/autoload.php';
 
-foreach (NewsManager::getInstance()->listNews() as $news) {
-	echo("############ NEWS " . $news->getTitle() . " ############\n");
-	echo($news->getBody() . "\n");
-	foreach (CommentManager::getInstance()->listComments() as $comment) {
-		if ($comment->getNewsId() == $news->getId()) {
-			echo("Comment " . $comment->getId() . " : " . $comment->getBody() . "\n");
-		}
-	}
+use Utils\CommentManager;
+use Utils\NewsManager;
+
+class Main
+{
+    private $newsManager;
+    private $commentManager;
+
+    public function __construct(NewsManager $newsManager, CommentManager $commentManager)
+    {
+        $this->newsManager = $newsManager;
+        $this->commentManager = $commentManager;
+    }
+
+    public function displayNewsAndComments()
+    {
+        $newsList = $this->newsManager->listNews();
+        $comments = $this->commentManager->listComments();
+
+        foreach ($newsList as $news) {
+            echo("############ NEWS " . $news->getTitle() . " ############\n");
+            echo($news->getBody() . "\n");
+
+            foreach ($comments as $comment) {
+                if ($comment->getNewsId() == $news->getId()) {
+                    echo("Comment " . $comment->getId() . " : " . $comment->getBody() . "\n");
+                }
+            }
+        }
+    }
 }
 
+//Instantiate the news manager & comment manager
+$newsManager = NewsManager::getInstance();
 $commentManager = CommentManager::getInstance();
-$c = $commentManager->listComments();
+
+//Create main object and displayNewsAndComments
+$main = new Main($newsManager, $commentManager);
+$main->displayNewsAndComments();

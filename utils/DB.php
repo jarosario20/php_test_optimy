f4d71b3a8ca4198a92056f4d5b3648489ccd5d36
Jay-Ar <jay.rosario024@gmail.com>
2024-05-16 00:20:23 +0800

    adding try catch and params to prevent sql injection

--- utils/DB.php
+++ utils/DB.php
@@ -1,42 +1,67 @@
 <?php
-
 class DB
 {
 	private $pdo;
 
 	private static $instance = null;
 
+	//private function to prevent the direct instantiation
 	private function __construct()
 	{
 		$dsn = 'mysql:dbname=phptest;host=127.0.0.1';
 		$user = 'root';
 		$password = 'pass';
 
-		$this->pdo = new \PDO($dsn, $user, $password);
+		try {
+			$this->pdo = new \PDO($dsn, $user, $password, array(
+						\PDO::ATTR_PERSISTENT => true
+					));
+			$this->pdo->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
+		} catch(\PDOException $e) {
+			throw new \Exception('Connection failed: ' . $e->getMessage());
+		}
 	}
 
-	public static function getInstance()
+	//For Singleton pattern and to get the single instance of the class
+	public static function getInstance(): self
 	{
 		if (null === self::$instance) {
-			$c = __CLASS__;
-			self::$instance = new $c;
+			self::$instance = new self();
 		}
 		return self::$instance;
 	}
 
-	public function select($sql)
+	//a public function or method to run select query
+	public function select($sql, $params = [])
 	{
-		$sth = $this->pdo->query($sql);
-		return $sth->fetchAll();
+		try {
+            $sth = $this->pdo->prepare($sql);
+            $sth->execute($params);
+            return $sth->fetchAll(\PDO::FETCH_ASSOC);
+        } catch (\PDOException $e) {
+            throw new \Exception('Select query failed: ' . $e->getMessage());
+        }
 	}
 
-	public function exec($sql)
-	{
-		return $this->pdo->exec($sql);
-	}
+	//a public function or method to run insert/update/delete query
+	public function exec($sql, $params = [])
+    {
+        try {
+            $sth = $this->pdo->prepare($sql);
+            $sth->execute($params);
+            return $sth->rowCount();
+        } catch (\PDOException $e) {
+            throw new \Exception('Exec query failed: ' . $e->getMessage());
+        }
+    }
 
+	//a public function or method to get the last inserted row
 	public function lastInsertId()
 	{
-		return $this->pdo->lastInsertId();
+        try {
+            return $this->pdo->lastInsertId();
+        } catch (\PDOException $e) {
+            throw new \Exception('Getting last insert ID failed: ' . $e->getMessage());
+        }
 	}
 }
